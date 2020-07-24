0c4ffce3dcf98fc870e320fc3f1cba3767852046
David Alves de Souza <alves.david@outlook.com>
2020-07-24 12:25:58 -0300

    Initial commit

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5e7587456a431acf5cda094694cae832'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '84ef9f44-93cf-4ded-869c-08acaa5267d7'
+};
